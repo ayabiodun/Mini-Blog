433fe88feff785d5b7576594ec4e5cdb71ad44b1
Ayomide Abiodun <ayomide.f.abiodun@gmail.com>
2026-08-17 08:02:19 +0100

    Added edit and delete functionality

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ const createForm = document.querySelector(".create-form") as HTMLFormElement;
 const createBtn = document.querySelector("#create-btn") as HTMLButtonElement;
 const cancelBtn = document.querySelector("#cancel-btn") as HTMLButtonElement;
 const modal = document.querySelector('#blog-modal') as HTMLDivElement;
+let blogBeingEdited: BlogInfo | null = null;
 
 window.addEventListener("pageshow", () => {
   const updatedBlogs = getBlogs();
@@ -27,15 +28,33 @@ if(createForm){
     const content = formData.get("content") as string;
     const imageUrl = formData.get("image") as string;
 
-    createBlog(title, content, description, imageUrl);
-    saveBlogs(blogData);
-    displayBlog();
-    createForm.reset();
+    if(blogBeingEdited){
+      blogBeingEdited.title = title;
+      blogBeingEdited.description = description;
+      blogBeingEdited.content = content;
+      blogBeingEdited.image = imageUrl;
+
+      saveBlogs(blogData);
+      displayBlog();
+      createForm.reset();
+      modal.style.display = "none";
+      blogBeingEdited = null;
+    }
+    else{
+      createBlog(title, content, description, imageUrl);
+      saveBlogs(blogData);
+      displayBlog();
+      createForm.reset();
+      modal.style.display = "none";
+    }
+    
   });
 }
 
 blogContainer.addEventListener("click", (e: Event) => {
   const target = e.target as HTMLElement;
+  const deleteBtn = target.closest(".delete-btn");
+  const editBtn = target.closest('.edit-btn');
   const clickedChild = target.closest(".blog-card");
 
   if (!clickedChild) return;
@@ -43,6 +62,37 @@ blogContainer.addEventListener("click", (e: Event) => {
   const clickedBlog = blogData.find((blog) => blog.id === clickedChild.id);
 
   if (!clickedBlog) return;
+  blogBeingEdited = clickedBlog;
+
+  if(editBtn){
+    const titleInput = createForm.elements.namedItem("title") as HTMLInputElement;
+    const descriptionInput = createForm.elements.namedItem("description") as HTMLTextAreaElement;
+    const contentInput = createForm.elements.namedItem("content") as HTMLTextAreaElement;
+    const imageInput = createForm.elements.namedItem("image") as HTMLInputElement;
+    const updateBtn = document.querySelector('.submit-btn') as HTMLButtonElement;
+
+    titleInput.value = clickedBlog.title;
+    descriptionInput.value = clickedBlog.description;
+    contentInput.value = clickedBlog.content;
+    imageInput.value = clickedBlog.image;
+    updateBtn.innerText = 'Update';
+
+    modal.style.display = "block";
+    return;
+  }
+  if (deleteBtn) {
+    const shouldDelete = confirm("Are you sure you want to delete this blog?");
+
+    if (!shouldDelete) return;
+
+    const index = blogData.indexOf(clickedBlog);
+    blogData.splice(index, 1);
+
+    saveBlogs(blogData);
+    displayBlog();
+
+    return;
+  }
 
   window.location.href = `/blog.html?id=${clickedBlog.id}`;
 });
@@ -86,6 +136,10 @@ function displayBlog(blogs: BlogInfo[] = blogData) {
                 <div class="card-footer">
                   <span>${formatDate(blog.date)}</span><br>
                   <span>${blog.views} views <i class="fa-solid fa-eye"></i></span>
+                </div>
+                <div class="interact">
+                  <button class="edit-btn">Edit</button>
+                  <button class="delete-btn">Delete</button>
                 </div>
               `;
 
